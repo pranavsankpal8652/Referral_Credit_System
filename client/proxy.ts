094c4c93037e47d806e6c1192a83b8db7178197c
Pranav Sankpal <162540013+pranavsankpal8652@users.noreply.github.com>
2025-11-12 17:46:35 +0530

    production fixing

--- client/proxy.ts
+++ client/proxy.ts
@@ -1,18 +1,18 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function proxy(request: NextRequest) {
-//   const session = request.cookies.get("authToken")?.value || null;
-//   // console.log("session" + session);
-//   //   If no session, redirect to login
-//   if (!session) {
-//     // console.log("no session");
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
+export function proxy(request: NextRequest) {
+  const session = request.cookies.get("authToken")?.value || null;
+  console.log("session" + session);
+  //   If no session, redirect to login
+  // if (!session) {
+  //   // console.log("no session");
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
-//   // Allow request to continue
-//   return NextResponse.next();
-// }
+  // // Allow request to continue
+  // return NextResponse.next();
+}
 
 // export const config = {
 //   matcher: ["/dashboard/:path*"], // Protect dashboard and subroutes
